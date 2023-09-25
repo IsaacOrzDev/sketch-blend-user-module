c6b3922b215ae0ee06b462b6e7fc922e55e73a6a
Isaac <isaachkdeveloper@gmail.com>
2023-09-26 00:15:09 +0800

    feat: Generate one time token

--- Services/AccessTokenService.cs
+++ Services/AccessTokenService.cs
@@ -14,33 +14,38 @@ public class AccessTokenServiceGrpc : AccessTokenService.AccessTokenServiceBase
 
   private readonly ILogger<UserServiceGrpc> _logger;
   private readonly IConfiguration _config;
+  private Db.AppDbContext _appDbContext;
 
-  public AccessTokenServiceGrpc(ILogger<UserServiceGrpc> logger, IConfiguration config)
+  public AccessTokenServiceGrpc(ILogger<UserServiceGrpc> logger, IConfiguration config, Db.AppDbContext appDbContext)
   {
     _logger = logger;
     _config = config;
+    _appDbContext = appDbContext;
   }
 
-  public override Task<AccessTokenReply> GenerateAccessToken(GenerateAccessTokenRequest request, ServerCallContext context)
+  private AccessTokenReply GenerateAccessToken(dynamic dto)
   {
     var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("JWT_SECRET") ?? ""));
     var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.Aes128CbcHmacSha256);
 
     dynamic data = new ExpandoObject();
-    data.ImageUrl = request.ImageUrl ?? "";
-    data.Id = request.UserId;
+    data.ImageUrl = dto.ImageUrl ?? "";
+    data.Id = dto.UserId;
 
 
     var claims = new[]
     {
-      new Claim(ClaimTypes.NameIdentifier, request.Username ?? ""),
-      new Claim(ClaimTypes.Email, request.Email ?? ""),
+      new Claim(ClaimTypes.NameIdentifier, dto.Username ?? ""),
+      new Claim(ClaimTypes.Email, dto.Email ?? ""),
       new Claim(ClaimTypes.UserData, JsonConvert.SerializeObject(data)),
     };
     var expiresAt = DateTime.Now;
 
-    switch (request.DurationType)
+    switch (dto.DurationType)
     {
+      case "10m":
+        expiresAt = expiresAt.AddMinutes(10);
+        break;
       case "1d":
       default:
         expiresAt = expiresAt.AddDays(1);
@@ -55,14 +60,15 @@ public class AccessTokenServiceGrpc : AccessTokenService.AccessTokenServiceBase
 
     var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
 
-    return Task.FromResult(new AccessTokenReply()
+    var reply = new AccessTokenReply
     {
       AccessToken = tokenStr,
       ExpiresAtUtc = Timestamp.FromDateTime(expiresAt.ToUniversalTime())
-    });
+    };
+    return reply;
   }
 
-  public override Task<VerifyAccessTokenReply> VerifyAccessToken(VerifyAccessTokenRequest request, ServerCallContext context)
+  private VerifyAccessTokenReply VerifyAccessToken(dynamic dto)
   {
     var tokenHandler = new JwtSecurityTokenHandler();
     var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetValue<string>("JWT_SECRET") ?? ""));
@@ -79,7 +85,9 @@ public class AccessTokenServiceGrpc : AccessTokenService.AccessTokenServiceBase
 
     try
     {
-      var claimsPrincipal = tokenHandler.ValidateToken(request.AccessToken, validationParameters, out var validatedToken);
+
+      SecurityToken validatedToken = null;
+      var claimsPrincipal = tokenHandler.ValidateToken(dto.AccessToken, validationParameters, out validatedToken);
       var jwtToken = (JwtSecurityToken)validatedToken;
       var username = jwtToken.Claims.FirstOrDefault(x => x.Type == ClaimTypes.NameIdentifier)?.Value;
       var email = jwtToken.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
@@ -87,21 +95,101 @@ public class AccessTokenServiceGrpc : AccessTokenService.AccessTokenServiceBase
 
       var role = jwtToken.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Role)?.Value;
 
-      return Task.FromResult(new VerifyAccessTokenReply()
+      return new VerifyAccessTokenReply()
       {
         IsValid = true,
         UserId = data.Id,
         Username = username,
         Email = email,
         ImageUrl = data.ImageUrl,
-      });
+      };
     }
     catch (Exception)
     {
-      return Task.FromResult(new VerifyAccessTokenReply()
+      return new VerifyAccessTokenReply()
+      {
+        IsValid = false
+      };
+    }
+  }
+
+  public override Task<AccessTokenReply> GenerateAccessToken(GenerateAccessTokenRequest request, ServerCallContext context)
+  {
+    dynamic dto = new ExpandoObject();
+    dto.Username = request.Username;
+    dto.Email = request.Email;
+    dto.ImageUrl = request.ImageUrl;
+    dto.UserId = request.UserId;
+    dto.DurationType = request.DurationType;
+    var reply = this.GenerateAccessToken(dto);
+
+    return Task.FromResult(reply);
+  }
+
+  public override Task<VerifyAccessTokenReply> VerifyAccessToken(VerifyAccessTokenRequest request, ServerCallContext context)
+  {
+
+    dynamic dto = new ExpandoObject();
+    dto.AccessToken = request.AccessToken;
+    var reply = VerifyAccessToken(dto);
+    return Task.FromResult(reply);
+  }
+
+  public override async Task<AddOneTimeAccessTokenReply> AddOneTimeAccessToken(AddOneTimeAccessTokenRequest request, ServerCallContext context)
+  {
+    dynamic dto = new ExpandoObject();
+    dto.Username = request.Username;
+    dto.Email = request.Email;
+    dto.ImageUrl = request.ImageUrl;
+    dto.UserId = request.UserId;
+    dto.DurationType = "10m";
+    var reply = GenerateAccessToken(dto);
+
+    var oneTimeToken = new Db.OneTimeAccessToken
+    {
+      Email = request.Email,
+      Token = reply.AccessToken,
+      Username = request.Username,
+    };
+
+    _appDbContext.OneTimeAccessTokens.Add(oneTimeToken);
+    await _appDbContext.SaveChangesAsync();
+
+    return new AddOneTimeAccessTokenReply
+    {
+      AccessToken = reply.AccessToken,
+      ExpiresAtUtc = reply.ExpiresAtUtc,
+    };
+  }
+
+  public override Task<VerifyOneTimeAccessTokenReply> VerifyOneTimeAccessToken(VerifyOneTimeAccessTokenRequest request, ServerCallContext context)
+  {
+
+    var record = _appDbContext.OneTimeAccessTokens.FirstOrDefault(x => x.Token == request.AccessToken);
+    if (record == null)
+    {
+      return Task.FromResult(new VerifyOneTimeAccessTokenReply
       {
         IsValid = false
       });
     }
+    else
+    {
+      _appDbContext.OneTimeAccessTokens.Remove(record);
+      _appDbContext.SaveChanges();
+    }
+
+    dynamic dto = new ExpandoObject();
+    dto.AccessToken = request.AccessToken;
+    var reply = VerifyAccessToken(dto);
+
+    return Task.FromResult(new VerifyOneTimeAccessTokenReply
+    {
+      IsValid = reply.IsValid,
+      UserId = reply.UserId,
+      Username = reply.Username,
+      Email = reply.Email,
+      ImageUrl = reply.ImageUrl,
+    });
   }
 }
